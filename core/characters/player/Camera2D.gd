4d5983a9d33896ad76eda88c5aba95204b06a486
Diego Alejandro Ochoa Gutierrez <diego.ochoa@globant.com>
2019-09-10 15:30:20 -0500

    Improves on Camera2D functionality

--- core/characters/player/Camera2D.gd
+++ core/characters/player/Camera2D.gd
@@ -1,12 +1,13 @@
 extends Camera2D
 
 const ZOOM_RATE: Vector2 = Vector2(0.5, 0.5)
-const MAX_ZOOM: Vector2 =  Vector2(1.4, 1.4)
-const NORMAL_ZOOM: Vector2 = Vector2(1, 1)
+const MAX_ZOOM: Vector2 =  Vector2(2.2, 2.2)
+const NORMAL_ZOOM: Vector2 = Vector2(1.7, 1.7)
+var is_bullet_time_on: bool = false
 
 func _process(delta: float) -> void:
 	
-	if Input.is_action_pressed("super_speed"):
+	if Input.is_action_pressed("super_speed") and not is_bullet_time_on:
 		if zoom <= MAX_ZOOM:
 			zoom += ZOOM_RATE * delta
 	else:
