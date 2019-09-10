27c9ae027b5aea4c451e42c538b07c8d0ad50e1a
Diego Alejandro Ochoa Gutierrez <diego.ochoa@globant.com>
2019-09-10 15:38:23 -0500

    Add several improvements
    Better handling of screens
    Better game flow
    Refractor code for easier reading

--- game/Game.gd
+++ game/Game.gd
@@ -1,57 +1,80 @@
 extends Node2D
-onready var sprite = $ParallaxBackground/ParallaxLayer/Sprite
-onready var parallax_layer = $ParallaxBackground/ParallaxLayer
-onready var paralax_bg = $ParallaxBackground
+
+onready var stats_screen = $CanvasLayer/Stats
+onready var score_screen = $CanvasLayer/Score
+onready var minimap = $CanvasLayer/Minimap
 onready var start_screen = $CanvasLayer/StartScreen
 onready var game_over_screen = $CanvasLayer/GameOverScreen
-export var player_path: NodePath
-var player
-var scroll = Vector2(0,3) 
+onready var message_screen = $CanvasLayer/MessageScreen
+onready var player: PackedScene = preload("res://core/characters/player/Player.tscn")
 
 func _ready() -> void:
 	
-	game_over_screen.hide()
-	parallax_layer.motion_mirroring = sprite.texture.get_size().rotated(sprite.global_rotation)
-	player = get_node(player_path)
-	GameManager.spawn_point_container = $SpawnPointContainer
-	get_tree().paused = true
-
-func _process(delta: float) -> void:	
-	
-	if player != null:
-    	scroll += player.velocity / 200
-	paralax_bg.scroll_offset += scroll
+	before_new_game()	
 
 func _unhandled_key_input(event: InputEventKey) -> void:
 	
-	if event.is_action_pressed("ui_cancel"):
-		get_tree().quit()
+	GameManager.quit_game(event)
 
-func _on_Player_player_died() -> void:
+func _on_Event_player_died() -> void:
+	
+	end_game()
+
+func _on_StartScreen_start_button_pressed() -> void:
+	
+	start_new_game()
+
+func _on_StartScreen_exit_button_pressed() -> void:
+	
+	GameManager.quit_game()
+
+func _on_GameOverScreen_exit_button_pressed() -> void:
+	
+	GameManager.quit_game()
+
+func _on_GameOverScreen_restart_button_pressed() -> void:
+	
+	restart_game()
+	
+func on_Event_message_sent(_text: String) -> void:
+	
+	message_screen.set_message(_text)
+	message_screen.show()
+	yield(get_tree().create_timer(3.0), "timeout")
+	message_screen.hide()
+
+func before_new_game() -> void:
+	
+	start_screen.show()
+	game_over_screen.hide()
+	score_screen.hide()
+	stats_screen.hide()
+	minimap.hide()
+	message_screen.hide()
+	get_tree().paused = true
+	Event.connect("message_sent", self, "on_Event_message_sent")
+	
+func start_new_game() -> void:
+		
+	GameManager.player = player.instance()
+	add_child(GameManager.player)
+	Event.connect("player_died", self, "_on_Event_player_died")
+	GameManager.stats_ui = stats_screen
+	GameManager.spawn_point_container = $SpawnPointContainer
+	GameManager.start_game()
+	start_screen.hide()
+	score_screen.show()
+	stats_screen.show()
+	minimap.show()
+	
+func end_game() -> void:
 	
 	yield(get_tree().create_timer(1.0), "timeout")
 	game_over_screen.set_score(GameManager.score_ui.score)
 	game_over_screen.show()
-	get_tree().paused = true
+	GameManager.end_game()	
 
-func _on_StartScreen_start_button_pressed() -> void:
+func restart_game() -> void:
 	
-	start_screen.hide()
-	get_tree().paused = false
-
-func _on_StartScreen_exit_button_pressed() -> void:
-	
-	get_tree().quit()
-
-func _on_GameOverScreen_exit_button_pressed() -> void:
-	
-	get_tree().quit()
-
-func _on_GameOverScreen_restart_button_pressed() -> void:
-	
-	for s in $SpawnPointContainer.get_children():
-		s.delete_all_enemies()
 	game_over_screen.hide()
-	player.restart()	
-	GameManager.score_ui.set_score(0)
-	get_tree().paused = false
+	GameManager.restart_game()
