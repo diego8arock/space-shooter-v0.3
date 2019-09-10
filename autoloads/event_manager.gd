888dc509ccb9bd154c4460eae800cbca5b0260a2
Diego Alejandro Ochoa Gutierrez <diego.ochoa@globant.com>
2019-09-10 15:28:56 -0500

    Add event manager
    Manages scene-cross events

---
+++ autoloads/event_manager.gd
@@ -0,0 +1,22 @@
+extends Node
+
+#warning-ignore:unused_signal
+signal player_died()
+
+#warning-ignore:unused_signal
+signal slow_down_time()
+
+#warning-ignore:unused_signal
+signal normal_time()
+
+#warning-ignore:unused_signal
+signal enemy_destroyed(_position)
+
+#warning-ignore:unused_signal
+signal message_sent(_text)
+
+#warning-ignore:unused_signal
+signal power_up_picked_up(_type)
+
+#warning-ignore:unused_signal
+signal health_restored()
