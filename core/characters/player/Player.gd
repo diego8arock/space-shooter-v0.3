8abba566f6e21b2fdbc04131f29cf8d49a83ce0d
Diego Alejandro Ochoa Gutierrez <diego.ochoa@globant.com>
2019-09-10 15:36:52 -0500

    Add several improvements to player
    Add slowdown power
    Fixed bugs
    Refractor some code

--- core/characters/player/Player.gd
+++ core/characters/player/Player.gd
@@ -1,18 +1,17 @@
 extends Actor
+class_name Player
 
 onready var explosion: PackedScene = preload("res://effects/small_explosion/SmallExplosion.tscn")
-
-export var ui_path: NodePath
-var ui
-
 onready var ship_sprite = $Sprite
 onready var collision = $CollisionShape2D
 onready var camera = $Camera2D
-onready var indicators = $Indicators
 onready var trail = $Trail
 onready var health = $Health
 onready var debug = $Debug
+onready var turret = $Turret
+onready var thruster = $Sprite/Thruster
 
+#movement
 var direction: Vector2 = Vector2(1, 0)
 var velocity: Vector2
 var rotation_speed: float = 2.0
@@ -27,15 +26,17 @@ var max_super_speed: float = 3.0
 var min_super_speed: float = 1.0
 var super_speed_acceleration: float = 2.5
 
-signal player_died()
+#time
+var is_bullet_time_on: bool = false
+var max_bullet_Time: float = 100.0
+var min_bullet_time: float = 0.0
+var bullet_time_use_rate: float = 0.5
+var bullet_time_recharge_rate: float = 0.3
+var current_bullet_time: float
 
 func _ready() -> void:
 	
-	ui = get_node(ui_path)
-	health.set_max_health(100.0)
-	health.set_full_health()
-	debug.add_property("max_super_speed")
-	debug.add_property("current_health", health)
+	start()
 
 func _process(delta: float) -> void:
 
@@ -53,24 +54,51 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("player_right"):
 		velocity_rotation += rotation_speed * delta		
 		
-	if Input.is_action_pressed("super_speed"):
+	if Input.is_action_pressed("super_speed") and not is_bullet_time_on:
 		curren_super_speed += super_speed_acceleration * delta
-		curren_super_speed = clamp(curren_super_speed, min_super_speed, max_super_speed)
 	else:
 		curren_super_speed -= super_speed_acceleration * delta
-		curren_super_speed = clamp(curren_super_speed, min_super_speed, max_super_speed)		
+	curren_super_speed = clamp(curren_super_speed, min_super_speed, max_super_speed)	
+		
+	if is_bullet_time_on:
+		current_bullet_time -= bullet_time_use_rate	
+	else:
+		current_bullet_time += bullet_time_recharge_rate
+	current_bullet_time = clamp(current_bullet_time, min_bullet_time, max_bullet_Time)
+	GameManager.stats_ui.set_energy(current_bullet_time)
+	
+	if is_bullet_time_on and current_bullet_time == min_bullet_time:
+		is_bullet_time_on = false
 		
 func _physics_process(delta: float) -> void:
 	
-	velocity = direction.normalized().rotated(velocity_rotation) * current_speed * curren_super_speed
+	velocity = direction.normalized().rotated(velocity_rotation) * Time.adjust_speed(current_speed) * Time.adjust_speed(curren_super_speed)
 	ship_sprite.global_rotation = velocity_rotation + deg2rad(90)
 	collision.global_rotation = velocity_rotation
 	move_and_collide(velocity)
 	
+func _input(event: InputEvent) -> void:
+	
+	if event is InputEventKey:
+		if event.is_action_pressed("super_speed"):
+			turret.using_super_speed = true
+		if event.is_action_released("super_speed"):
+			turret.using_super_speed = false
+			
+	if event is InputEventMouseButton:
+		if event.is_action_pressed("bullet_time"):
+			if not is_bullet_time_on:
+				Event.emit_signal("slow_down_time")
+				is_bullet_time_on = true
+			else:
+				Event.emit_signal("normal_time")
+				is_bullet_time_on = false
+			camera.is_bullet_time_on = is_bullet_time_on
+	
 func take_damage(_damage: float) -> void:
 	
 	health.take_damage(_damage)	
-	ui.set_progress(health.get_current_health())
+	GameManager.stats_ui.set_health(health.get_current_health())
 
 func _on_Health_health_depleated() -> void:
 	
@@ -81,7 +109,16 @@ func died() -> void:
 	hide()
 	trail.hide()
 	WeaponManager.show_small_explosion(global_position, global_scale * 1.5)
-	emit_signal("player_died")
+	Event.emit_signal("player_died")
+	
+func start() -> void:
+	
+	health.set_max_health(100.0)
+	health.set_full_health()
+	debug.add_property("max_super_speed")
+	debug.add_property("current_health", health)
+	current_bullet_time = max_bullet_Time
+	Event.connect("health_restored", self, "on_Event_health_restore")
 	
 func restart() -> void:
 	
@@ -96,3 +133,8 @@ func restart() -> void:
 	health.set_full_health()
 	yield(get_tree().create_timer(1.0), "timeout")
 	trail.show()
+
+func on_Event_health_restore() -> void:
+	
+	health.set_full_health()
+	GameManager.stats_ui.set_health(health.get_current_health())
