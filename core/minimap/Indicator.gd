56309d629420e523ff4a26ea5d90ad5c59bcb90c
Diego Alejandro Ochoa Gutierrez <diego.ochoa@globant.com>
2019-09-10 15:33:52 -0500

    Improves on minimap indicator functionality

--- core/minimap/Indicator.gd
+++ core/minimap/Indicator.gd
@@ -4,7 +4,6 @@ onready var sprite = $Sprite
 onready var debug = $Debug
 
 var radius
-var player: Actor
 var enemy: Actor
 var out_of_range: float = 4000.0
 
@@ -15,21 +14,17 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	
 	if EnemyManager.is_enemy_alive(enemy):				
-		var distance = player.global_position.distance_to(enemy.global_position)
+		var distance = GameManager.player.global_position.distance_to(enemy.global_position)
 		if distance <= out_of_range:
 			distance = distance / 20
 			sprite.position = Vector2(distance , 0)
 		else:
 			sprite.position = Vector2(radius , 0)
-		rotation = player.global_position.angle_to_point(enemy.global_position) + deg2rad(90)						
+		rotation = GameManager.player.global_position.angle_to_point(enemy.global_position) + deg2rad(90)						
 	
 func assing_radius(_radius: float) -> void:
 	
 	radius = _radius	
-
-func assing_player(_player: Actor) -> void:
-	
-	player = _player
 
 func assing_enemy(_enemy: Actor) -> void:
 	
