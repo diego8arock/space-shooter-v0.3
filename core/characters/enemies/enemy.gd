ac27f24a660374cb0be68159d3fff4b6e1f1f37e
Diego Alejandro Ochoa Gutierrez <diego.ochoa@globant.com>
2019-09-10 15:35:24 -0500

    Add several improvements for enemy
    Add time slowdown
    Adjust format
    Add destroyed signal

--- core/characters/enemies/enemy.gd
+++ core/characters/enemies/enemy.gd
@@ -29,14 +29,16 @@ var points: int
 #state
 enum STATE {IDLE, SEEK, PURSUIT}
 var state_name = ["IDLE", "SEEK", "PURSUIT", "EVADE"]
-var state 
+var state
 
 #signals
+#warning-ignore:unused_signal
 signal screen_entered()
+#warning-ignore:unused_signal
 signal screen_exited()
 
 func _ready() -> void:
-	
+
 	var m = movement as Movement
 	max_force = m.max_force
 	mass = m.mass
@@ -47,54 +49,56 @@ func _ready() -> void:
 	circle_radius = m.circle_radius
 	angle_change = m.angle_change
 	wander_rotation_speed = m.wander_rotation_speed
-	
+
 	var d = data as CoreData
 	collision_damage = d.collision_damage
 	points = d.points
 
 func seek(_target_position: Vector2) -> Vector2:
-	
-	desired_velocity = (_target_position - global_position).normalized() * max_seek_speed
+
+	desired_velocity = (_target_position - global_position).normalized() * Time.adjust_speed(max_seek_speed)
 	steer(desired_velocity - velocity)
-	return (velocity + steering).clamped(max_seek_speed)
-	
+	return (velocity + steering).clamped(Time.adjust_speed(max_seek_speed))
+
 func pursuit(_target_position: Vector2, _target_velocity: Vector2) -> Vector2:
-	
+
 	var distance = _target_position - global_position
-	var T = distance.length() / max_pursuit_speed
+	var T = distance.length() / Time.adjust_speed(max_pursuit_speed)
 	future_position = _target_position + _target_velocity * T
 	steer(seek(future_position))
-	return (velocity + steering).clamped(max_pursuit_speed)
+	return (velocity + steering).clamped(Time.adjust_speed(max_pursuit_speed))
 
 func wander(_delta) -> Vector2:
-	
+
 	var circle_center: Vector2 = velocity
 	circle_center = circle_center.normalized()
 	circle_center *= cirlce_distance
-		
+
 	var displacement: Vector2 = Vector2(0, -1)
 	displacement *= circle_radius
-		
+
 	var rad_wander_angle = deg2rad(wander_angle)
 	var length = displacement.length()
 	displacement.x = cos(rad_wander_angle) * length
 	displacement.y = sin(rad_wander_angle) * length
-		
+
 	randomize()
 	wander_angle += randf() * angle_change - angle_change * 0.5
-		
-	var wander_force: Vector2 = circle_center + displacement
-		
-	steer(wander_force)		
-	return (velocity + steering).clamped(max_wander_speed)
 
-func steer(_start: Vector2) -> void:	
-	
+	var wander_force: Vector2 = circle_center + displacement
+
+	steer(wander_force)
+	return (velocity + steering).clamped(Time.adjust_speed(max_wander_speed))
+
+func steer(_start: Vector2) -> void:
+
 	steering = _start
 	steering = steering.clamped(max_force)
 	steering /= mass
 
 func died() -> void:
-	
-	hide()	
+
+	hide()
+	Event.emit_signal("enemy_destroyed", global_position)
 	EnemyManager.delete_enemy(self)
+	
