0e1ef59b5f4f73369b649e4ec0da51a77669a5b1
Diego Alejandro Ochoa Gutierrez <diego.ochoa@globant.com>
2019-09-18 13:46:26 -0500

    Add big explosion

--- autoloads/weapon_manager.gd
+++ autoloads/weapon_manager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var small_explosion: PackedScene = preload("res://effects/small_explosion/SmallExplosion.tscn")
+var big_explosion: PackedScene = preload("res://effects/big_explosion/BigExplosion.tscn")
 
 var weapon_container: Node2D
 
@@ -16,6 +17,13 @@ func show_explosion(_explosion, _position, _scale) -> void:
 func show_small_explosion(_position: Vector2, _scale: Vector2) -> void:
 	
 	var new_explosion = small_explosion.instance()
+	new_explosion.global_position = _position
+	new_explosion.global_scale = _scale
+	weapon_container.add_child(new_explosion)
+	
+func show_big_explosion(_position: Vector2, _scale: Vector2) -> void:
+	
+	var new_explosion = big_explosion.instance()
 	new_explosion.global_position = _position
 	new_explosion.global_scale = _scale
 	weapon_container.add_child(new_explosion)
