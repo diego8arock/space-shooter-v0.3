28a7f67f969ed545178d2ce2cca6bdf337f2427b
Diego Alejandro Ochoa Gutierrez <diego.ochoa@globant.com>
2019-09-18 13:50:26 -0500

    Improve bullet behaviour

--- core/weapons/Bullet.gd
+++ core/weapons/Bullet.gd
@@ -1,30 +1,42 @@
 extends Area2D
 
-onready var explosion: PackedScene = preload("res://effects/bullet_explosion/BulletExplosion.tscn")
+onready var timer = $Timer
+onready var explosion_effect: PackedScene = preload("res://effects/bullet_explosion/BulletExplosion.tscn")
+onready var debug = $Debug
+
 var velocity: Vector2
 var max_speed: float = 300.0
-var speed_multiplier: float = 3.0
+var speed_multiplier: float = 4.0
 var damage: float = 0.0
+
+func _ready() -> void:
+	
+	debug.add_property("velocity")
 
 func _physics_process(delta: float) -> void:
 	
-	global_position += velocity * delta
+	global_position += Time.adjust_velocity(velocity) * delta
 	
 func shoot(_position: Vector2, _rotation: Vector2, _damage: float) -> void:
 	
 	global_position = _position
 	global_rotation = _rotation.angle()
 	damage = _damage
-	velocity = _rotation * Time.adjust_speed(max_speed) * speed_multiplier
+	velocity = _rotation * max_speed * speed_multiplier
 	WeaponManager.add(self)
 
 func _on_Timer_timeout() -> void:
 	
+	debug.delete_all_properties()
 	call_deferred("free")
 
 func _on_Bullet_body_entered(body: PhysicsBody2D) -> void:
 	
 	if body is Actor and body.health.get_current_health() != 0.0:
-		WeaponManager.show_explosion(explosion.instance(), body.global_position, global_scale)
+		timer.stop()
+		set_physics_process(false)
+		hide()
+		WeaponManager.show_explosion(explosion_effect.instance(), global_position, global_scale * 2)
 		body.take_damage(damage)	
+		debug.delete_all_properties()
 		call_deferred("free")
