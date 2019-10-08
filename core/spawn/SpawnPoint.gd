4f7e3592c968cf106f11010006b316a63d7f266a
Diego <diego.ochoa@globant.com>
2019-10-08 16:22:06 -0500

    Adjust spawn point parameters for testing

--- core/spawn/SpawnPoint.gd
+++ core/spawn/SpawnPoint.gd
@@ -27,25 +27,26 @@ func _ready() -> void:
 		TIER.ONE:			
 			timer.wait_time = rand_range(4.0, 6.0)
 			timer.start()
-			max_enemies = 3
-			enemy = homing
+			max_enemies = 1
+			enemy = fighter
 		TIER.TWO:
 			timer.wait_time = rand_range(4.0, 6.0)
 			timer.start()
 			max_enemies = 1
-			enemy = homing
+			enemy = fighter
 	
 func _on_Timer_timeout() -> void:
 
 	if container.get_child_count() == max_enemies:
 		return
 
-	var new_enemy = enemy.instance()
+	var new_enemy: Enemy = enemy.instance()
 	container.add_child(new_enemy)
 	new_enemy.global_position = global_position + Vector2(50, 50)
 	new_enemy.global_rotation = ray.global_rotation
 	new_enemy.wander_angle = rad2deg(global_rotation)
 	new_enemy.global_scale = Vector2(0.5, 0.5)
+	new_enemy.location = GameManager.chekpoint_container.get_closest_checkpoint(new_enemy.global_position)
 	EnemyManager.add_enemies(new_enemy)
 	timer.wait_time = rand_range(4.0, 6.0)
 	timer.start()
